62a19884ec22c486b77f071404d6a1695681a56b
BaheshtFaiq360 <baheshtfaiq9@gmail.com>
2025-07-09 16:14:03 +0500

    Add files via upload

---
+++ area of circle.cpp.cpp
@@ -0,0 +1,9 @@
+# include<iostream>
+using namespace std;
+int main(){
+   float r = 4.5;
+   float a = 3.145*r*r;
+   cout<< "area of the circle is:";
+   cout<<a;
+
+}
---
+++ triangle sides.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+      int a, b, c;
+cout <<"Enter your fist number : ";
+cin >> a;
+cout <<"Enter your 2nd number : ";
+cin >> b;
+cout <<"Enter your 3rd number : ";
+cin >> c;
+      if((a+b)>c && (b+c)>a and (c+a)>b){
+      	cout<<"Valid TRIANGLE .";
+      	
+	  }
+      
+      else{
+      	cout<<"Invalid TRIANGLE .";
+	  }
+      
+      
+      
+      
+}
